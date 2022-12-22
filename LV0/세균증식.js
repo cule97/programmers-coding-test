e8fc8ebe27234cc0121ef7a03098de37edfa66ad
cule97 <wltjd326@naver.com>
2022-12-22 22:56:28 +0900

    feat:solved

---
+++ LV0/세균증식.js
@@ -0,0 +1,17 @@
+function solution(n, t) {
+  let result=n
+  for(i=1;i<=t;i++){
+      result=result*2
+      }
+  return result
+  }
+
+  function solution(n, t) {
+    return n << t;
+  }
+
+
+function solution(n, t) {
+
+    return n*Math.pow(2,t);
+}
