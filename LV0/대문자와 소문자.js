825c57750852cdc6d6143fe4954fede738ae3efd
cule97 <wltjd326@naver.com>
2022-12-25 23:47:40 +0900

    feat:solved

--- LV0/대문자와 소문자.js
+++ LV0/대문자와 소문자.js
@@ -9,11 +9,10 @@ function solution(my_string) {
   let b=''
   let a=my_string.toLowerCase();
   for(i=0;i<my_string.length;i++){
-  if(my_string[i]===a[i]){
-     b=b+my_string[i].toUpperCase()
-  }
-  else b=b+ my_string[i].toLowerCase();
- 
-}
+    if(my_string[i]===a[i]){
+       b=b+my_string[i].toUpperCase()
+    }
+     else b=b+ my_string[i].toLowerCase();
+    }
    return b
 }
