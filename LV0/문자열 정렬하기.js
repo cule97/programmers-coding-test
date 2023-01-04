6c7a16d1148001a63c707681c5dbdbce1ee84252
cule97 <wltjd326@naver.com>
2023-01-04 12:46:03 +0900

    feat:solved

--- LV0/문자열 정렬하기.js
+++ LV0/문자열 정렬하기.js
@@ -1,4 +1,4 @@
 function solution(my_string) {
-  let a= my_string.split('').filter((a)=>!Number.isNaN(+a)).map(a => +a).sort((a,b)=>        a - b)
+  let a= my_string.split('').filter((a)=>!Number.isNaN(+a)).map(a => +a).sort((a,b)=> a - b)
    return a
 }
